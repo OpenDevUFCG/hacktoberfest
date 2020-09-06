d0de222c5d595b160792afaab6a4afeca72cb41a
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-09-06 20:36:48 -0300

    Adição de 0s em números menores que 10 no timer

--- docs/js/countdown_timer.js
+++ docs/js/countdown_timer.js
@@ -6,22 +6,35 @@ const timer = setInterval(function() {
 
     const distance = eventDate - currentDate;
 
-    const [days, hours, minutes, seconds] = convertDistance(distance);
+    const { days, hours, minutes, seconds } = convertDistance(distance);
+
 
     document.getElementById("countdown-timer").textContent = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
     if (distance < 0) {
         clearInterval(timer);
-        document.getElementById("countdown-timer").textContent = "e vaamos de"
+        document.getElementById("countdown-timer").textContent = "É hoje!"
     }
 
 }, 1000);
 
 function convertDistance(distance) {
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    return [days, hours, minutes, seconds];
+    const timeNumbers = { days, hours, minutes, seconds };
+    console.log(timeNumbers)
+
+    for (key in timeNumbers) {
+        const timeNumber = timeNumbers[key];
+        if (timeNumber < 10) {
+            timeNumbers[key] = `0${timeNumber}`
+        }
+        console.log()
+    }
+        
+
+    return timeNumbers;
 }
