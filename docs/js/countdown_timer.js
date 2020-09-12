930b47f2b71bfda7f2b4188620fd13aa4f216a1e
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-09-12 19:57:12 -0300

    Feitas as mudanças requisitas

--- docs/js/countdown_timer.js
+++ docs/js/countdown_timer.js
@@ -1,41 +1,44 @@
+var interval = 0
 
-const eventDate = new Date("October 7, 2020 00:00:00").getTime();
-
-const timer = setInterval(function() {
-    const currentDate = new Date().getTime();
-    const distance = eventDate - currentDate;
-
-    const { days, hours, minutes, seconds } = convertDistance(distance);
-
-    document.getElementById("days").textContent = days
-    document.getElementById("hours").textContent = hours
-    document.getElementById("minutes").textContent = minutes
-    document.getElementById("seconds").textContent = seconds
-
-    if (distance < 0) {
-        clearInterval(timerFormat);
-        document.getElementById("countdown-timer").textContent = "É hoje!"
-    }
-
-}, 1000);
-
-function convertDistance(distance) {
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    const timeNumbers = { days, hours, minutes, seconds };
-    console.log(timeNumbers)
-
-    for (key in timeNumbers) {
-        const timeNumber = timeNumbers[key];
-        if (timeNumber < 10) {
-            timeNumbers[key] = `0${timeNumber}`
-        }
-        console.log()
-    }
-        
-
-    return timeNumbers;
+calculateInterval = () => {
+    const startDate = new Date()
+    const endDate = new Date("October 01, 2020 00:00:00")
+    interval = endDate - startDate
 }
+
+calculateTime = () => {
+    const days = Math.floor(interval / (1000*3600*24))
+    const hours = Math.floor(interval % (1000*3600*24) / (1000 * 3600))
+    const minutes = Math.floor(interval % (1000*3600*24) % (1000 * 3600) / (1000 * 60))
+    const seconds = Math.floor(interval % (1000*3600*24) % (1000 * 3600) % (1000 * 60) / 1000)
+
+    return {days, hours, minutes, seconds}
+} 
+
+startTimer = () => {
+    calculateInterval()
+    displayTimer()
+
+    setInterval(displayTimer, 1000);
+}
+
+displayTimer = () => {
+    const { days, hours, minutes, seconds } = calculateTime()
+
+    document.getElementById('days').innerHTML = fillZeroes(days)
+    document.getElementById('hours').innerHTML = fillZeroes(hours)
+    document.getElementById('minutes').innerHTML = fillZeroes(minutes)
+    document.getElementById('seconds').innerHTML = fillZeroes(seconds)
+}
+
+
+fillZeroes = (value) => {
+    value += ""
+
+    if(value.length==1)
+        return "0" + value
+    else
+        return value
+}
+
+startTimer()
