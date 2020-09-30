2f4d8b49e9c8eda3b8f4b9580a7bad11f5e58a50
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2020-09-30 20:10:06 -0300

    Adição de estrutura base pra a seção de faq

---
+++ docs/data/frequently_questions.js
@@ -0,0 +1,31 @@
+const questions = [
+    {
+        question: "AQUI VAI UMA PERGUNTA MUITO INTERESSANTE COM UMA LINDA RESPOSTA SOBRE A VIDA, O UNIVERSO E TUDO mais!",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        question: "AQUI VAI UMA PERGUNTA MUITO INTERESSANTE COM UMA LINDA RESPOSTA SOBRE A VIDA, O UNIVERSO E TUDO mais!",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        question: "lorem ipsun?",
+        answer: "lorem ipsum"
+    },
+    {
+        question: "lorem ipsun?",
+        answer: "lorem"
+    },
+    {
+        question: "lorem ipsun?",
+        answer: "ipsum"
+    },
+    {
+        question: "lorem ipsun?",
+        answer: "lorem ipsum"
+    },
+    {
+        question: "lorem ipsun?",
+        answer: "lorem ipsum"
+    }
+]
+
---
+++ docs/js/app.js
@@ -0,0 +1,54 @@
+let current_page = 1
+const questions_per_page = 2
+const itemTemplate = document.querySelector('.faq-template').cloneNode(true);
+
+window.addEventListener('load', () => {
+    setListenerFunctions()
+    renderQuestionPage(current_page)
+})
+
+function setListenerFunctions(){
+    const prevBtn = document.querySelector('.prev')
+    const nextBtn = document.querySelector('.next')
+
+    //prevBtn.addEventListener('click', prev)
+    nextBtn.addEventListener('click', next)
+}
+
+function getNumPages(){
+    return Math.ceil(questions.length/ questions_per_page)
+}
+
+function prev(){
+    if(current_page > 1){
+        current_page--;
+        renderQuestionPage(current_page);
+    } 
+}
+
+function next(){
+    if(current_page < getNumPages()){
+        current_page++;
+        renderQuestionPage(current_page);
+    }
+}
+
+function renderQuestionPage(page){
+    const questionContainer = document.getElementById('faq-list');
+
+    questionContainer.innerHTML = "";
+
+    for(let i = (page-1) * questions_per_page; i < (page * questions_per_page); i++){
+        if(questions[i]){
+
+            const item = itemTemplate.cloneNode(true);
+            const question = item.querySelector('.question');
+            const answer = item.querySelector('.answer');
+    
+            question.innerHTML = questions[i].question;
+            answer.innerHTML = questions[i].answer;
+    
+            questionContainer.appendChild(item);
+        }
+    }
+}
