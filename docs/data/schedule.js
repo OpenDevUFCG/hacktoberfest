cea5870dc86c83d18487a6f1055b96a46826b37a
Jessé Souza <jesse.neto@ccc.ufcg.edu.br>
2020-10-02 14:35:33 -0300

    Versao provisoria contendo o cronograma

---
+++ docs/data/schedule.js
@@ -0,0 +1,153 @@
+const schedule = [
+    { 
+        title: "Abertura Hacktoberfest",
+        topic: "",
+        days: [
+            { 
+                date: "Sábado (3) - 14h",
+                events: [
+                    { 
+                        speaker: "Fanny Barbosa e Mariana Nascimento",
+                        topic: "Abertura + palestras de Git  e trilha open source",
+                    }
+                ]
+                
+            },
+        ],
+    },
+    { 
+        title: "1° Semana <br>(dias 5 e 10)",
+        topic: "Trilha Iniciantes",
+        days: [
+            { 
+                
+                date: "Terça (6) - 19h30",
+                events: [
+                    { 
+                        speaker: "Espíndula",
+                        topic: "Conceitos iniciais de programação",
+                    }
+                ] 
+            },
+            { 
+                date: "Sábado (3) - 14h",
+                events: [
+                    {
+                        speaker: "Héricles",
+                        topic: "OpenMined e contribuindo para organizações",
+                    }
+                ]  
+            }
+        ],
+    },
+    { 
+        title: "2° Semana <br>(dias 13 e 17)",
+        topic: "Trilha Backend",
+        days: [
+            { 
+                date: "Terça (13) - 19h30",
+                events: [
+                    {
+                        speaker: "Matheus Gaudêncio",
+                        topic: "Arquitetura em camadas",
+                    },
+                    {
+                        speaker: "Gabriel Tavares",
+                        topic: "Otimização de Código", 
+                    }
+                ] 
+            },
+            { 
+                date: "Sábado (17) - 14h",
+                events: [
+                    {
+                        speaker: "Rerisson Matos",
+                        topic: "Criando uma API utilizando NodeJS e banco de dados",
+                    }
+                ]  
+            },
+        ],
+    },
+    { 
+        title: "3° Semana <br>(dias 20, 22 e 24)",
+        topic: "Trilha Frontend",
+        days: [
+            { 
+                date: "Terça (20) - 19h30",
+                events: [
+                    {
+                        speaker: "Bea",
+                        topic: "Como estilizar uma página web",
+                    },
+                    {
+                        speaker: "Jessé",
+                        topic: "Introdução a Web",
+                    }
+                ]  
+            },
+            { 
+                date: "Quinta (29) - 19h30",
+                events: [
+                    {
+                        speaker: "Lucas de Medeiros",
+                        topic: "Introdução a React",
+                    },
+                    {
+                        speaker: "Luciano",
+                        topic: "Como gerenciar estados eficientimente",
+                    }
+                ]  
+            },
+            { 
+                date: "Sábado (31) - 14h",
+                events: [
+                    {
+                        speaker: "Thayanne",
+                        topic: "Como fazer um blog utilizando Gatsby",
+                    },
+                    {
+                        speaker: "Estácio Pereira",
+                        topic: "Introdução a PWA",
+                    }
+                ]  
+            },
+        ],
+    },
+    { 
+        title: "4° Semana <br>(dias 27, 29 e 31)",
+        topic: "Trilha Análise de Dados",
+        days: [
+            { 
+                date: "Terça (27) - 19h30",
+                events: [
+                    {
+                        speaker: "Hadrizia",
+                        topic: "Como fazer análises exploratórias de dados em Python",
+                    }
+                ]  
+            },
+            { 
+                date: "Quinta (29) - 19h30",
+                events: [
+                    {
+                        speaker: "Nazareno",
+                        topic: "Visualização de Dados",
+                    }
+                ]  
+            },
+            { 
+                date: "Sábado (31) - 14h",
+                events: [
+                    {
+                        speaker: "Júlio Barreto",
+                        topic: "Introdução a NLP",
+                    },
+                    {
+                        speaker: "Elloá",
+                        topic: "Palestra sobre ...",
+                    }
+                ]  
+            }
+        ],
+    }
+]
