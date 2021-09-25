cc245e921f82ff1d4158d02dbda755ed1e812317
marianacoimbra <mariana.coimbra@ccc.ufcg.edu.br>
2021-09-25 17:11:46 -0300

    add schedule information and theme change

--- docs/data/schedule.js
+++ docs/data/schedule.js
@@ -1,153 +1,104 @@
 const schedule = [
-    { 
-        title: "Abertura Hacktoberfest",
-        topic: "",
-        days: [
-            { 
-                date: "Sábado (3) - 14h",
-                events: [
-                    { 
-                        speaker: "Fanny Vieira e Mariana Nascimento",
-                        topic: "Abertura + palestras de Git e trilha open source",
-                    }
-                ]
-                
-            },
+  {
+    title: "Abertura Hacktoberfest",
+    topic: "",
+    days: [
+      {
+        date: "Sexta (1) ",
+        events: [
+          {
+            speaker: "Iele Passos",
+            topic: "Abertura do Evento",
+          },
         ],
-    },
-    { 
-        title: "1° Semana <br>(dias 6 e 10)",
-        topic: "Trilha Iniciantes",
-        days: [
-            { 
-                
-                date: "Terça (6) - 19h30",
-                events: [
-                    { 
-                        speaker: "Pedro Espíndula",
-                        topic: "Conceitos iniciais de programação",
-                    }
-                ] 
-            },
-            { 
-                date: "Sábado (10) - 14h",
-                events: [
-                    {
-                        speaker: "Héricles Emanuel",
-                        topic: "OpenMined e contribuindo para organizações",
-                    }
-                ]  
-            }
+      },
+    ],
+  },
+  {
+    title: "1° Semana <br>",
+    topic: "",
+    days: [
+      {
+        date: "Segunda (4) - 19h",
+        events: [
+          {
+            speaker: "Mari e Jaciane",
+            topic: "Minicurso git github - elas@cc",
+          },
         ],
-    },
-    { 
-        title: "2° Semana <br>(dias 13 e 17)",
-        topic: "Trilha Backend",
-        days: [
-            { 
-                date: "Terça (13) - 19h30",
-                events: [
-                    {
-                        speaker: "Matheus Gaudêncio",
-                        topic: "Cache, Stateless, Sharding e Load Balancer - Estratégias de escalonamento para sistemas corporativos",
-                    },
-                    {
-                        speaker: "Gabriel Tavares",
-                        topic: "Otimização de Código", 
-                    }
-                ] 
-            },
-            { 
-                date: "Sábado (17) - 14h",
-                events: [
-                    {
-                        speaker: "Rerisson Matos",
-                        topic: "Criando uma API utilizando NodeJS e banco de dados",
-                    }
-                ]  
-            },
+      },
+    ],
+  },
+  {
+    title: "2° Semana <br>(dias 14, 15 e 18)",
+    topic: "",
+    days: [
+      {
+        date: "Terça (14) - 19h",
+        events: [
+          {
+            speaker: "Sergio",
+            topic: "Linux for dummies",
+          },
         ],
-    },
-    { 
-        title: "3° Semana <br>(dias 20, 22 e 24)",
-        topic: "Trilha Frontend",
-        days: [
-            { 
-                date: "Terça (20) - 19h30",
-                events: [
-                    {
-                        speaker: "Bea",
-                        topic: "Como estilizar uma página web",
-                    },
-                    {
-                        speaker: "Jessé Souza",
-                        topic: "Introdução a Web",
-                    }
-                ]  
-            },
-            { 
-                date: "Quinta (22) - 19h30",
-                events: [
-                    {
-                        speaker: "Lucas de Medeiros",
-                        topic: "Introdução a React",
-                    },
-                    {
-                        speaker: "Luciano de Oliveira",
-                        topic: "Estado em React: os comos e os porquês",
-                    }
-                ]  
-            },
-            { 
-                date: "Sábado (24) - 14h",
-                events: [
-                    {
-                        speaker: "Thayanne Sousa",
-                        topic: "Como fazer um blog utilizando Gatsby",
-                    },
-                    {
-                        speaker: "Estácio Pereira",
-                        topic: "Introdução a PWA",
-                    }
-                ]  
-            },
+      },
+      {
+        date: "Sábado (15) - 19h",
+        events: [
+          {
+            speaker: "Sergio",
+            topic: "introdução ao terminal",
+          },
         ],
-    },
-    { 
-        title: "4° Semana <br>(dias 27, 29 e 31)",
-        topic: "Trilha Análise de Dados",
-        days: [
-            { 
-                date: "Terça (27) - 19h30",
-                events: [
-                    {
-                        speaker: "Hadrizia Santos",
-                        topic: "Introdução à Ciência de dados",
-                    }
-                ]  
-            },
-            { 
-                date: "Quinta (29) - 19h30",
-                events: [
-                    {
-                        speaker: "Nazareno",
-                        topic: "Visualização de Dados",
-                    }
-                ]  
-            },
-            { 
-                date: "Sábado (31) - 14h",
-                events: [
-                    {
-                        speaker: "Júlio Barreto",
-                        topic: "Textões e dados: o que é PLN e em que podemos utilizar",
-                    },
-                    {
-                        speaker: "Elloá B. Guedes",
-                        topic: "Machine Learning: Os Próximos Desafios",
-                    }
-                ]  
-            }
+      },
+      {
+        date: "Sábado (18) - 19h",
+        events: [
+          {
+            speaker: "Marta Lais",
+            topic: "Áreas da Infraestrutura",
+          },
         ],
-    }
-]
+      },
+    ],
+  },
+  {
+    title: "3° Semana <br>(dias 20 e 21)",
+    topic: "",
+    days: [
+      {
+        date: "Segunda (20) - 19h30",
+        events: [
+          {
+            speaker: "",
+            topic: "VTEX",
+          },
+        ],
+      },
+      {
+        date: "Terça (21) - 19h",
+        events: [
+          {
+            speaker: "Marcella Siqueira",
+            topic: "Lições de Escrita Criativa para Escrita Acadêmica",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "4° Semana <br>(dia 29)",
+    topic: "",
+    days: [
+      {
+        date: "Quarta (29)",
+        events: [
+          {
+            speaker: "Iele Passos",
+            topic: "Encerramento - sorteios",
+          },
+        ],
+      },
+    ],
+  },
+];
