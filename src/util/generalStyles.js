477d1bfbcfd5ac04e589a9454b17961f50f19bd9
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-10-13 17:22:45 -0300

    Adds spacial background

---
+++ src/util/generalStyles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  min-height: 100vh;
+  box-sizing: border-box;
+`;
+
+export const SectionTitle = styled.h2`
+  font-family: "Eater", cursive;
+  letter-spacing: 0.145rem;
+  color: #f8a362;
+`;
