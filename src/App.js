12c0ea52f85de5983aa0de1a188f2695f46890ac
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-10-15 17:41:51 -0300

    Adiciona seção 'Outras edições'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { AboutSection } from "./components/AboutSection";
+import { PhotoShoot } from "./components/PhotoShoot";
 import { Wrapper } from "./util/generalStyles";
 
 function App() {
   return (
     <Wrapper>
       <AboutSection />
+      <PhotoShoot />
     </Wrapper>
   );
 }
