c0399a052b0e4408ac9e093044abdad15a092ee7
leandra.silva <leandra.silva@ccc.ufcg.edu.br>
2022-10-18 18:40:24 -0300

    adiciona link de formulario de inscrição

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Button } from "./styles";
 
-export const Buttons = ({ color, text, link, disabled }) => {
+export const Buttons = ({ color, text, link}) => {
   return (
     <a href={link} target="_blank" rel="noreferrer">
-      <Button color={color} disabled={disabled}>
+      <Button color={color}>
         {text}
       </Button>
     </a>
